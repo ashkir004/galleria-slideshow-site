95327d8b30f285cf5d18253edabbd00496039a7f
ashkir004 <ashkir004@gmail.com>
2026-07-12 17:53:01 +0300

    add visual divider and raise artwork description above abit

--- src/lib/components/domain/ArtworkDescription.svelte
+++ src/lib/components/domain/ArtworkDescription.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-	import { cn } from "$lib/utils";
+	import { cn } from '$lib/utils';
 
 	let { data, class: className = '' } = $props();
 </script>
 
-<article class={cn("flex flex-col lg:items-end xl:items-center", className)}>
+<article class={cn('flex flex-col lg:items-end xl:items-center', className)}>
 	<span
-		class="text-8xl md:text-9xl xl:text-[9rem] text-gray-600/10 z-20 inline-block self-end md:self-start -translate-y-2/3 md:translate-y-3/4
+		class="text-8xl md:text-9xl xl:text-[9rem] text-gray-600/10 z-20 inline-block self-end md:self-start 
+					-translate-y-2/3 
+					md:translate-y-3/4
+					lg:translate-y-1/4
                     lg:self-end xl:self-center xl:translate-x-1/4">{data.item?.year}</span
 	>
 	<div
 		class="flex flex-col gap-16 md:max-w-md lg:max-w-xs md:self-center lg:self-end xl:self-center
-		-translate-y-1/4 md:translate-y-0"
+		-translate-y-1/4 md:translate-y-0 lg:-translate-y-1/9"
 	>
 		<p class="mt-auto text-sm text-gray-500 font-bold leading-7 tracking-tight text-balance">
 			{data.item?.description}
