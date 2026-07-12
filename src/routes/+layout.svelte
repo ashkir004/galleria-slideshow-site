f7d92fe898cab88905d84d34bc2c25589b47c886
ashkir004 <ashkir004@gmail.com>
2026-07-12 14:21:55 +0300

    remove bottom padding for the page, letting footer set it

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="p-7 flex flex-col gap-10 min-w-screen min-h-screen">
+<div class="px-7 pt-7 flex flex-col gap-10 min-w-screen min-h-screen">
 	<Header {data} />
 	{@render children()}
 </div>
